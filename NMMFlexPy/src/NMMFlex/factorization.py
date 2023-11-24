1ad9515e06789d08bb9679e69af1cd273c533d4b
crhisto <crhisto@email.com>
2023-11-24 11:42:22 -0500

    Update of the internal version

--- NMMFlexPy/src/NMMFlex/factorization.py
+++ NMMFlexPy/src/NMMFlex/factorization.py
@@ -7,7 +7,7 @@
 #         Kimura, and Hiroshi Sawada
 # License: Released under GNU Public License (GPL)
 
-__version__ = '0.1.11'
+__version__ = '0.1.12'
 __author__ = 'Crhistian Cardona <crhisto@gmail.com>'
 
 import math
